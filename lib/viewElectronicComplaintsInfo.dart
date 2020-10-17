a38d1593b586e016d168edc718cee71d2f948cdd
nouf771 <55227417+nouf771@users.noreply.github.com>
2020-10-17 19:23:15 +0300

    asking for help as front end

---
+++ lib/viewElectronicComplaintsInfo.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+//import 'package:flutter_email_sender/flutter_email_sender.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appName = 'البلاغ الالكتروني';
+
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: MyHomePage(
+        title: appName,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  viewElectronicComplaintsInfo createState() =>
+      new viewElectronicComplaintsInfo();
+}
+
+class viewElectronicComplaintsInfo extends State<MyHomePage> {
+  final nameController = TextEditingController();
+  final numberController = TextEditingController();
+  final emailController = TextEditingController();
+  final linkController = TextEditingController();
+
+  bool _isButtonDisabled;
+  @override
+  void initState() {
+    _isButtonDisabled = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: Center(child: Text(widget.title)),
+        ),
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Column(children: <Widget>[
+                Container(
+                    child: TextField(
+                  controller: numberController,
+                  textAlign: TextAlign.right,
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                      // border: OutlineInputBorder(),
+                      icon: const Icon(
+                        Icons.keyboard_return_sharp,
+                        color: Colors.blue,
+                      ),
+                      labelText: " رقم الطلب :" + " " + "نوف",
+                      labelStyle: TextStyle(color: Colors.black)),
+                  enabled: false,
+                )),
+                Container(
+                    child: TextField(
+                  textAlign: TextAlign.right,
+                  controller: nameController,
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                      // border: OutlineInputBorder(),
+                      icon: const Icon(
+                        Icons.person,
+                        color: Colors.blue,
+                      ),
+                      labelText: " الاسم :" + " " + "نوف",
+                      labelStyle: TextStyle(color: Colors.black)),
+                  enabled: false,
+                )),
+                Container(
+                  child: TextField(
+                    textAlign: TextAlign.right,
+                    controller: emailController,
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                        // border: OutlineInputBorder(),
+                        icon: const Icon(
+                          Icons.email,
+                          color: Colors.blue,
+                        ),
+                        labelText: " البريد الالكتروني :" + " " + "نوف",
+                        labelStyle: TextStyle(color: Colors.black)),
+                    enabled: false,
+                  ),
+                ),
+
+                Container(
+                    child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 2,
+                  controller: linkController,
+                  textAlign: TextAlign.right,
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                      // border: OutlineInputBorder(),
+                      icon: const Icon(
+                        Icons.link,
+                        color: Colors.blue,
+                      ),
+                      labelText: " الرابط :" + " " + "نوف",
+                      labelStyle: TextStyle(color: Colors.black)),
+                  enabled: false,
+                )),
+                SizedBox(height: 90), //to add space between colmun
+                ButtonTheme(
+                  //padding: Ed
+                  // geInsets.all(-50),
+                  minWidth: 200.0,
+                  height: 40.0,
+                  child: RaisedButton(
+                    onPressed: _isButtonDisabled ? null : _checker,
+                    child: Text(
+                      "اغلاق البلاغ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ])),
+        )));
+  }
+
+  void _helpButtonPressed() async {
+    setState(() {
+      _isButtonDisabled = true;
+    });
+    await Firebase.initializeApp();
+    final databaseReference = FirebaseFirestore.instance;
+    DocumentSnapshot doc = await databaseReference
+        .collection('helpRequests')
+        .doc('totalRequest')
+        .get();
+    int id = doc.get('autoNumber');
+    databaseReference
+        .collection('helpRequests')
+        .doc('totalRequest')
+        .update({'autoNumber': FieldValue.increment(1)});
+    DocumentReference ref =
+        await databaseReference.collection("helpRequests").add({
+      'name': nameController.text,
+      'email': emailController.text,
+      'requestId': id,
+      'response': '',
+      'status': 'opened',
+    });
+    print(ref.id);
+    Alert(
+      context: context,
+      title: "تم الإرسال بنجاح",
+      desc: "رقم الطلب :  " + id.toString(),
+      buttons: [
+        DialogButton(
+          child: Text(
+            "حسناً",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context), // هنا وين يروح بعدها ؟
+          color: Colors.lightBlue[800],
+          radius: BorderRadius.circular(0.5),
+        ),
+      ],
+    ).show();
+  }
+
+  void _checker() {
+    bool flag = true;
+    if (nameController.text.isEmpty) {
+      //
+      _showMyDialog("غير مكتمل", "الرجاء كتابة الأسم بشكل صحيح");
+      flag = false;
+      return;
+    }
+    if (!(isValidEmail(emailController.text))) {
+      //
+      _showMyDialog("غير مكتمل", "الرجاء كتابة البريد الإلكتروني بشكل صحيح");
+      flag = false;
+      return;
+    }
+
+    if (flag) {
+      _helpButtonPressed();
+    }
+  }
+
+  bool isValidEmail(String input) {
+    final RegExp regex = new RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+    return regex.hasMatch(input);
+  }
+
+/*
+  void _sendEmail() async {
+    final Email email = Email(
+      body: 'Email body',
+      subject: 'Email subject',
+      recipients: ['reemalmalki98@gmail.com'],
+     // attachmentPaths: ['/path/to/attachment.zip'],
+      isHTML: false,
+    );
+
+    await FlutterEmailSender.send(email);
+  }
+*/
+  Future<void> _showMyDialog(String title, String body) async {
+    Alert(
+      context: context,
+      title: title,
+      desc: body,
+      buttons: [
+        DialogButton(
+          child: Text(
+            "حسناً",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context), // هنا وين يروح بعدها ؟
+          color: Colors.lightBlue[800],
+          radius: BorderRadius.circular(0.5),
+        ),
+      ],
+    ).show();
+  }
+}
